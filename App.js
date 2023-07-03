22e2b64a4ed044ade41df219db6fa428076e7a00
ლიკა გოგიშვილი <lgogishvili@geostat.gov.ge>
2023-07-03 09:22:28 +0400

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text style={styles.text}>შტერო</Text>
+      <Button title='tab me' />
+    </View> 
   );
 }
 
@@ -17,4 +16,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'red',
+    fontSize: 100
+
+  }
 });
